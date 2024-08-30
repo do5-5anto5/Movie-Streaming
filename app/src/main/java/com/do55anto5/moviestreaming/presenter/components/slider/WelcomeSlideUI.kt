2f4e3c86c9cc8e8825593dde7199783de758197a
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-08-30 16:36:10 -0300

    Feat: encapsulate HorizontalPager inside a Column, and call SlideIndicatorUI inside WelcomeSlideUI - this separate slide animation from pager text, slider indicator will "be the same"

--- app/src/main/java/com/do55anto5/moviestreaming/presenter/components/slider/WelcomeSlideUI.kt
+++ app/src/main/java/com/do55anto5/moviestreaming/presenter/components/slider/WelcomeSlideUI.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.do55anto5.moviestreaming.R
+import com.do55anto5.moviestreaming.presenter.components.indicator.SlideIndicatorUI
 import com.do55anto5.moviestreaming.presenter.theme.MovieStreamingTheme
 import com.do55anto5.moviestreaming.presenter.theme.UrbanistFamily
 
@@ -33,59 +34,80 @@ fun WelcomeSlideUI(
     slideItems: List<Pair<String, String>>,
     pagerState: PagerState
 ) {
-    HorizontalPager(
-        state = pagerState,
-        Modifier
+
+    Column(
+        modifier = modifier
             .fillMaxSize(),
-        pageContent = {
-            Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        content = {
+
+            HorizontalPager(
+                state = pagerState,
                 Modifier
                     .fillMaxSize()
-                    .padding(24.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Bottom,
-                content = {
-                    Text(
-                        text = slideItems[pagerState.currentPage].first,
-                        style = TextStyle(
-                            fontSize = 40.sp,
-                            lineHeight = 48.sp,
-                            fontFamily = UrbanistFamily,
-                            fontWeight = FontWeight.Bold,
-                            color = MovieStreamingTheme.colorScheme.whiteColor,
-                            textAlign = TextAlign.Center
-                        )
-                    )
+                    .weight(1f),
+                pageContent = {
+                    Column(
+                        Modifier
+                            .fillMaxSize()
+                            .padding(24.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Bottom,
+                        content = {
+                            Text(
+                                text = slideItems[pagerState.currentPage].first,
+                                style = TextStyle(
+                                    fontSize = 40.sp,
+                                    lineHeight = 48.sp,
+                                    fontFamily = UrbanistFamily,
+                                    fontWeight = FontWeight.Bold,
+                                    color = MovieStreamingTheme.colorScheme.whiteColor,
+                                    textAlign = TextAlign.Center
+                                )
+                            )
 
-                    Spacer(modifier = Modifier.height(24.dp))
+                            Spacer(modifier = Modifier.height(24.dp))
 
-                    Text(
-                        text = slideItems[pagerState.currentPage].second,
-                        style = TextStyle(
-                            fontSize = 18.sp,
-                            lineHeight = 26.2.sp,
-                            fontFamily = UrbanistFamily,
-                            fontWeight = FontWeight.Medium,
-                            color = MovieStreamingTheme.colorScheme.whiteColor,
-                            textAlign = TextAlign.Center,
-                            letterSpacing = 0.2.sp
-                        )
+                            Text(
+                                text = slideItems[pagerState.currentPage].second,
+                                style = TextStyle(
+                                    fontSize = 18.sp,
+                                    lineHeight = 26.2.sp,
+                                    fontFamily = UrbanistFamily,
+                                    fontWeight = FontWeight.Medium,
+                                    color = MovieStreamingTheme.colorScheme.whiteColor,
+                                    textAlign = TextAlign.Center,
+                                    letterSpacing = 0.2.sp
+                                )
+                            )
+                        }
                     )
                 }
+            )
+
+            SlideIndicatorUI(
+                modifier = Modifier
+                    .padding(bottom = 24.dp),
+                totalIndicators = slideItems.size,
+                currentIndicator = pagerState.currentPage
             )
         }
     )
 }
 
 @OptIn(ExperimentalFoundationApi::class)
-@Preview(showBackground = true,
-    backgroundColor = 0xFF000000)
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFF000000
+)
 @Composable
 private fun WelcomeSlideUIPreview() {
     val slideItems = listOf(
         Pair(
             stringResource(id = R.string.welcome_slider_title),
-            stringResource(id = R.string.welcome_slider_slide1)),
+            stringResource(id = R.string.welcome_slider_slide1)
+        ),
         Pair(
             stringResource(id = R.string.welcome_slider_title),
             stringResource(id = R.string.welcome_slider_slide2)
