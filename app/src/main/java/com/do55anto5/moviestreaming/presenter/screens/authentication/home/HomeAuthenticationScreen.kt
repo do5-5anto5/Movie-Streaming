c08d04f877f8644bbfffdf1121f4a82b8ed31442
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-08-31 21:26:06 -0300

    Feat: improve HomeAuthenticationScreen structure with a new Column and a new Row. - the row has a link to Sign Up screen

--- app/src/main/java/com/do55anto5/moviestreaming/presenter/screens/authentication/home/HomeAuthenticationScreen.kt
+++ app/src/main/java/com/do55anto5/moviestreaming/presenter/screens/authentication/home/HomeAuthenticationScreen.kt
@@ -2,12 +2,15 @@ package com.do55anto5.moviestreaming.presenter.screens.authentication.home
 
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -41,80 +44,126 @@ fun HomeAuthenticationContent() {
             Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(paddingValues)
-                    .padding(24.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
+                    .padding(paddingValues),
                 content = {
-                    Image(
-                        painter = painterResource(id = R.drawable.placeholder_home_authentication),
-                        contentDescription = null
-                    )
-
-                    Spacer(modifier = Modifier.height(24.dp))
-
-                    Text(
-                        text = stringResource(id = R.string.label_title_authentication_screen),
-                        style = TextStyle(
-                            fontSize = 48.sp,
-                            lineHeight = 57.6.sp,
-                            fontFamily = UrbanistFamily,
-                            fontWeight = FontWeight.Bold,
-                            color = MovieStreamingTheme.colorScheme.textColor,
-                            textAlign = TextAlign.Center
-                        )
-                    )
-
-                    Spacer(modifier = Modifier.height(24.dp))
-
-                    SocialButton(
+                    Column(
                         modifier = Modifier
-                            .fillMaxWidth(),
-                        icon = painterResource(id = R.drawable.ic_google),
-                        isLoading = false,
-                        text = "Continue with Google",
-                        onClick = {}
+                            .weight(1f)
+                            .padding(24.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        content = {
+                            Image(
+                                painter = painterResource(id = R.drawable.placeholder_home_authentication),
+                                contentDescription = null
+                            )
+
+                            Spacer(modifier = Modifier.height(24.dp))
+
+                            Text(
+                                text = stringResource(id = R.string.label_title_authentication_screen),
+                                style = TextStyle(
+                                    fontSize = 48.sp,
+                                    lineHeight = 57.6.sp,
+                                    fontFamily = UrbanistFamily,
+                                    fontWeight = FontWeight.Bold,
+                                    color = MovieStreamingTheme.colorScheme.textColor,
+                                    textAlign = TextAlign.Center
+                                )
+                            )
+
+                            Spacer(modifier = Modifier.height(24.dp))
+
+                            SocialButton(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                icon = painterResource(id = R.drawable.ic_google),
+                                isLoading = false,
+                                text = "Continue with Google",
+                                onClick = {}
+                            )
+
+                            Spacer(modifier = Modifier.height(16.dp))
+
+                            SocialButton(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                icon = painterResource(id = R.drawable.ic_facebook),
+                                isLoading = false,
+                                text = "Continue with Facebook",
+                                onClick = {}
+                            )
+
+                            Spacer(modifier = Modifier.height(16.dp))
+
+                            SocialButton(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                icon = painterResource(id = R.drawable.ic_github),
+                                isLoading = false,
+                                text = "Continue with Github",
+                                onClick = {}
+                            )
+
+                            Spacer(modifier = Modifier.height(16.dp))
+
+                            HorizontalDividerWithText(
+                                text = stringResource(id = R.string.label_or_authentication_screen)
+                            )
+
+                            Spacer(modifier = Modifier.height(16.dp))
+
+                            PrimaryButton(
+                                text = stringResource(id = R.string.label_sign_with_password_authentication_screen),
+                                isLoading = false,
+                                onClick = {}
+                            )
+                        }
                     )
 
                     Spacer(modifier = Modifier.height(16.dp))
 
-                    SocialButton(
+                    Row(
                         modifier = Modifier
-                            .fillMaxWidth(),
-                        icon = painterResource(id = R.drawable.ic_facebook),
-                        isLoading = false,
-                        text = "Continue with Facebook",
-                        onClick = {}
-                    )
+                            .fillMaxWidth()
+                            .padding(24.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center,
+                        content = {
+                            Text(
+                                text = stringResource(id = R.string.label_sign_up_account_authentication_screen),
+                                style = TextStyle(
+                                    fontSize = 14.sp,
+                                    lineHeight = 19.6.sp,
+                                    fontFamily = UrbanistFamily,
+                                    color = MovieStreamingTheme.colorScheme.textColor,
+                                    textAlign = TextAlign.Right,
+                                    letterSpacing = 0.2.sp
+                                )
+                            )
 
-                    Spacer(modifier = Modifier.height(16.dp))
+                            Spacer(modifier = Modifier.width(8.dp))
 
-                    SocialButton(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        icon = painterResource(id = R.drawable.ic_github),
-                        isLoading = false,
-                        text = "Continue with Github",
-                        onClick = {}
-                    )
-
-                    Spacer(modifier = Modifier.height(16.dp))
-
-                    HorizontalDividerWithText(
-                        text = stringResource(id = R.string.label_or_authentication_screen)
-                    )
-
-                    Spacer(modifier = Modifier.height(16.dp))
-
-                    PrimaryButton(
-                        text = stringResource(id = R.string.label_sign_with_password_authentication_screen),
-                        isLoading = false,
-                        onClick = {}
+                            Text(
+                                text = stringResource(id = R.string.label_sign_up_authentication_screen),
+                                style = TextStyle(
+                                    fontSize = 14.sp,
+                                    lineHeight = 19.6.sp,
+                                    fontFamily = UrbanistFamily,
+                                    fontWeight = FontWeight.SemiBold,
+                                    color = MovieStreamingTheme.colorScheme.defaultColor,
+                                    textAlign = TextAlign.Right,
+                                    letterSpacing = 0.2.sp
+                                )
+                            )
+                        }
                     )
                 }
             )
         }
     )
 }
+
+
 
 @PreviewLightDark
 @Composable
