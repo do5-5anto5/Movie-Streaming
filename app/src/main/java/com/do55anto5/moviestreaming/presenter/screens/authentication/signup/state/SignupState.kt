1fd40ebd04f1a1e947af0f60695f3140a80d89f6
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-09-04 20:42:06 -0300

    Feat: expect passwordVisibility in SignupState constructor

--- app/src/main/java/com/do55anto5/moviestreaming/presenter/screens/authentication/signup/state/SignupState.kt
+++ app/src/main/java/com/do55anto5/moviestreaming/presenter/screens/authentication/signup/state/SignupState.kt
@@ -1,7 +1,8 @@
 package com.do55anto5.moviestreaming.presenter.screens.authentication.signup.state
 
 data class SignupState(
+    val isLoading: Boolean = false,
     val email: String = "",
     val password: String = "",
-    val isLoading: Boolean = false
+    val passwordVisibility: Boolean = false
 )
