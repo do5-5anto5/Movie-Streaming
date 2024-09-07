c146ad4cab8cea9b5127a9c4afcc1283f592a7ed
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-09-07 17:10:06 -0300

    Feat: implement user creation with email and password inside SignupRepositoryImpl

--- app/src/main/java/com/do55anto5/moviestreaming/data/remote/SignupRepositoryImpl.kt
+++ app/src/main/java/com/do55anto5/moviestreaming/data/remote/SignupRepositoryImpl.kt
@@ -2,13 +2,23 @@ package com.do55anto5.moviestreaming.data.remote
 
 import com.do55anto5.moviestreaming.domain.remote.repository.authentication.SignupRepository
 import com.google.firebase.auth.FirebaseAuth
+import kotlin.coroutines.suspendCoroutine
 
 class SignupRepositoryImpl(
     private val auth: FirebaseAuth
 ) : SignupRepository {
 
     override suspend fun register(email: String, password: String) {
-
+        return suspendCoroutine { continuation ->
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        // handle register state
+                    } else {
+                        // handle register state
+                    }
+                }
+        }
     }
 
 }
