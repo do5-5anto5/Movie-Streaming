18d5881e1a0e9f387cc2c172d5ea4659e47ee3c6
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-09-08 16:13:15 -0300

    Refactor - simplify code calling FirebaseHelper inside SignupRepositoryImpl

--- app/src/main/java/com/do55anto5/moviestreaming/data/remote/repository/authentication/SignupRepositoryImpl.kt
+++ app/src/main/java/com/do55anto5/moviestreaming/data/remote/repository/authentication/SignupRepositoryImpl.kt
@@ -1,16 +1,15 @@
 package com.do55anto5.moviestreaming.data.remote.repository.authentication
 
+import com.do55anto5.moviestreaming.core.helper.FirebaseHelper
 import com.do55anto5.moviestreaming.domain.remote.repository.authentication.SignupRepository
-import com.google.firebase.auth.FirebaseAuth
 import kotlin.coroutines.suspendCoroutine
 
-class SignupRepositoryImpl(
-    private val auth: FirebaseAuth
-) : SignupRepository {
+class SignupRepositoryImpl : SignupRepository {
 
     override suspend fun register(email: String, password: String) {
+
         return suspendCoroutine { continuation ->
-            auth.createUserWithEmailAndPassword(email, password)
+            FirebaseHelper.getAuth().createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener { task ->
                     if (task.isSuccessful) {
                         continuation.resumeWith(Result.success(Unit))
