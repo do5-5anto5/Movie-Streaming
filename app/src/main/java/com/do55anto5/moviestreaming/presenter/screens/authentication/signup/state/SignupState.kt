6281c2db982cbb36117ff8ea3b551fc9e7918afc
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-09-08 18:22:19 -0300

    Improve - change errorMessage: String? to a Pair<FeedbackType, Int>?

--- app/src/main/java/com/do55anto5/moviestreaming/presenter/screens/authentication/signup/state/SignupState.kt
+++ app/src/main/java/com/do55anto5/moviestreaming/presenter/screens/authentication/signup/state/SignupState.kt
@@ -1,5 +1,7 @@
 package com.do55anto5.moviestreaming.presenter.screens.authentication.signup.state
 
+import com.do55anto5.moviestreaming.core.enums.feedback.FeedbackType
+
 data class SignupState(
     val isLoading: Boolean = false,
     val email: String = "",
@@ -7,5 +9,5 @@ data class SignupState(
     val passwordVisibility: Boolean = false,
     val enableSignupButton: Boolean = false,
     val hasError: Boolean = false,
-    val errorMessage: Int? = null
+    val feedbackUI: Pair<FeedbackType, Int>? = null
 )
