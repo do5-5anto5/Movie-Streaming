55601f17857b505daca48ada23359501d59d9e5b
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-09-08 16:22:08 -0300

    Update - modules: - create a factory for UserRepository in repositoryModule - create a factory for SaveUserUseCase in useCaseModule - get saveUserUseCase presenterModule

--- app/src/main/java/com/do55anto5/moviestreaming/di/UseCaseModule.kt
+++ app/src/main/java/com/do55anto5/moviestreaming/di/UseCaseModule.kt
@@ -1,10 +1,13 @@
 package com.do55anto5.moviestreaming.di
 
 import com.do55anto5.moviestreaming.domain.remote.usecase.authentication.RegisterUseCase
+import com.do55anto5.moviestreaming.domain.remote.usecase.user.SaveUserUseCase
 import org.koin.dsl.module
 
 val useCaseModule = module {
 
     factory { RegisterUseCase(repository = get()) }
+
+    factory { SaveUserUseCase(repository = get()) }
 
 }
