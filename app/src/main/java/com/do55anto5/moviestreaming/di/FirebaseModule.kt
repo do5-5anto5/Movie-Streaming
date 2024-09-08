86310d011314cafa7d723ed29cb24028804d5aa8
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-09-08 16:06:02 -0300

    Refactor - remove unnecessary FirebaseModule.kt; and optimize imports at MainApplication

--- app/src/main/java/com/do55anto5/moviestreaming/di/FirebaseModule.kt
+++
@@ -1,8 +0,0 @@
-package com.do55anto5.moviestreaming.di
-
-import com.google.firebase.auth.FirebaseAuth
-import org.koin.dsl.module
-
-val firebaseModule = module {
-    single<FirebaseAuth> { FirebaseAuth.getInstance() }
-}
