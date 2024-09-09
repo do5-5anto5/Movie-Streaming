4f3b0485c227d1f0e593ece74928e725747cf9d1
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-09-08 22:20:30 -0300

    Feat show snackbar in SignupScreen - set the message in LaunchedEffect grants that only will appear when a determined error happens - use personalized FeedbackUI as content inside SnackbarHost inside Scaffold

--- app/src/main/java/com/do55anto5/moviestreaming/presenter/screens/authentication/signup/screen/SignupScreen.kt
+++ app/src/main/java/com/do55anto5/moviestreaming/presenter/screens/authentication/signup/screen/SignupScreen.kt
@@ -19,12 +19,19 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -43,12 +50,14 @@ import com.do55anto5.moviestreaming.presenter.components.button.PrimaryButton
 import com.do55anto5.moviestreaming.presenter.components.button.SocialButton
 import com.do55anto5.moviestreaming.presenter.components.divider.HorizontalDividerWithText
 import com.do55anto5.moviestreaming.presenter.components.editText.TextFieldUI
+import com.do55anto5.moviestreaming.presenter.components.snackbar.FeedbackUI
 import com.do55anto5.moviestreaming.presenter.components.topAppBar.TopAppBarUI
 import com.do55anto5.moviestreaming.presenter.screens.authentication.signup.action.SignupAction
 import com.do55anto5.moviestreaming.presenter.screens.authentication.signup.state.SignupState
 import com.do55anto5.moviestreaming.presenter.screens.authentication.signup.viewmodel.SignupViewModel
 import com.do55anto5.moviestreaming.presenter.theme.MovieStreamingTheme
 import com.do55anto5.moviestreaming.presenter.theme.UrbanistFamily
+import kotlinx.coroutines.launch
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -73,10 +82,45 @@ fun SignupContent(
     onBackPressed: () -> Unit
 ) {
 
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    LaunchedEffect(state.hasError) {
+        if (state.hasError) {
+            scope.launch {
+                val result = snackbarHostState
+                    .showSnackbar(
+                        message = context.getString(
+                            state.feedbackUI?.second ?: R.string.error_generic
+                        )
+                    )
+
+                if (result == SnackbarResult.Dismissed) {
+                    action(SignupAction.ResetErrorState)
+                }
+
+            }
+        }
+    }
+
     Scaffold(
         topBar = {
             TopAppBarUI(
                 onClick = {}
+            )
+        },
+        snackbarHost = {
+            SnackbarHost(
+                hostState = snackbarHostState,
+                snackbar = { snackbarData ->
+                    state.feedbackUI?.let { feedbackUI ->
+                        FeedbackUI(
+                            message = snackbarData.visuals.message,
+                            type = feedbackUI.first
+                        )
+                    }
+                }
             )
         },
         content = { paddingValues ->
