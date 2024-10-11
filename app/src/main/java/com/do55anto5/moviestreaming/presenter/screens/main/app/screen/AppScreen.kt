ccc20c3bfd5d7268034c0c00f8dca1c12f9fad58
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-10-11 15:13:59 -0300

    feat: implement AppScreen - in there, all main navigation screens content will be shown

---
+++ app/src/main/java/com/do55anto5/moviestreaming/presenter/screens/main/app/screen/AppScreen.kt
@@ -0,0 +1,57 @@
+package com.do55anto5.moviestreaming.presenter.screens.main.app.screen
+
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.do55anto5.moviestreaming.core.navigation.hosts.bottombar.BottomAppBarNavHost
+import com.do55anto5.moviestreaming.presenter.components.bottom_bar.BottomBarUI
+import com.do55anto5.moviestreaming.presenter.theme.MovieStreamingTheme
+
+@Composable
+fun AppScreen() {
+    AppContent()
+}
+
+@Composable
+private fun AppContent() {
+    val navController = rememberNavController()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+    Scaffold(
+        bottomBar = {
+            BottomBarUI(
+                currentDestination = currentDestination,
+                onItemClick = {
+                    navController.navigate(it.route) {
+                        popUpTo(navController.graph.startDestinationId) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        },
+        content = { paddingValues ->
+            BottomAppBarNavHost(
+             modifier = Modifier.padding(paddingValues),
+             navHostController = navController
+            )
+        }
+    )
+}
+
+@PreviewLightDark
+@Composable
+private fun AppScreenPreview() {
+    MovieStreamingTheme {
+        AppContent()
+    }
+}
