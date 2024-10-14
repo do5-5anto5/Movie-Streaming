05f0aa9b92f4928a1ae039a47fa40cab77bc06f1
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-10-14 17:01:31 -0300

    feat: add constants - till now it has just FILE_NAME AND WELCOME_VISITED constants

---
+++ app/src/main/java/com/do55anto5/moviestreaming/core/constants/Constants.kt
@@ -0,0 +1,6 @@
+package com.do55anto5.moviestreaming.core.constants
+
+object SharedPreferenceKeys {
+    const val FILE_NAME = "DEFAULT_PREFERENCES"
+    const val WELCOME_VISITED = "WELCOME_VISITED"
+}
