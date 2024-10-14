c991aaca184e79305f323542c122d4fae606737a
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-10-14 17:09:08 -0300

    feat: implement SplashViewModel

---
+++ app/src/main/java/com/do55anto5/moviestreaming/presenter/screens/splash/viewmodel/SplashViewModel.kt
@@ -0,0 +1,38 @@
+package com.do55anto5.moviestreaming.presenter.screens.splash.viewmodel
+
+
+import androidx.lifecycle.ViewModel
+import com.do55anto5.moviestreaming.core.helper.FirebaseHelper
+import com.do55anto5.moviestreaming.core.preferences.AppPreferences
+import com.do55anto5.moviestreaming.presenter.screens.splash.action.SplashAction
+import com.do55anto5.moviestreaming.presenter.screens.splash.state.SplashState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class SplashViewModel(
+    private val appPreferences: AppPreferences
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(SplashState())
+    val state = _state.asStateFlow()
+
+    fun submitAction(action: SplashAction) {
+        when (action) {
+            SplashAction.OnNextScreen -> {
+                getWelcomeVisited()
+            }
+        }
+    }
+
+    private fun getWelcomeVisited() {
+        _state.update { currentState ->
+            currentState.copy(
+                isWelcomeVisited = appPreferences.getWelcomeVisited(),
+                isLoading = false,
+                isAuthenticated = FirebaseHelper.isAuthenticated()
+            )
+        }
+    }
+
+}
