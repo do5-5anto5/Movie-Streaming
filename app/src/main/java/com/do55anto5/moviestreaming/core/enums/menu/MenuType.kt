d4e7c2f905d61883f7a707730e78196e64847a2b
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-10-24 18:52:45 -0300

    feat: implement MenuItems

---
+++ app/src/main/java/com/do55anto5/moviestreaming/core/enums/menu/MenuType.kt
@@ -0,0 +1,13 @@
+package com.do55anto5.moviestreaming.core.enums.menu
+
+enum class MenuType {
+    EDIT_PROFILE,
+    NOTIFICATION,
+    DOWNLOAD,
+    SECURITY,
+    LANGUAGE,
+    DARK_MODE,
+    HELP_CENTER,
+    PRIVACY_POLICY,
+    LOGOUT
+}
